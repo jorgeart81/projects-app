9c6cdc44fc41ce3d8c53ce17902f6ef132e8edde
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-06-06 17:56:55 -0500

    Create ProjectLayout

---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TopMenu from '@/modules/common/components/TopMenu.vue'
+import SideMenu from '@/modules/projects/components/SideMenu.vue'
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <!-- Top menu -->
+    <top-menu />
+    <main>
+      <!-- Sidemenu -->
+      <side-menu />
+
+      <router-view />
+    </main>
+  </div>
+</template>
