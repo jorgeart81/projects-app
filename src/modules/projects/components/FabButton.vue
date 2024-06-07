b99561ae7cc49b5feb6e410e6c1d7bfea2d2cdb5
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-06-07 14:37:17 -0500

    Use slot in FabButton

---
+++ src/modules/projects/components/FabButton.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface Props {
+  position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+}
+
+withDefaults(defineProps<Props>(), {
+  position: 'bottom-right'
+})
+
+defineEmits(['click'])
+</script>
+
+<template>
+  <button @click="$emit('click')" :class="['btn btn-circle btn-secondary p-[2px] fixed', position]">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.top-left {
+  @apply top-10 left-10;
+}
+
+.top-right {
+  @apply top-10 right-10;
+}
+
+.bottom-left {
+  @apply bottom-10 left-10;
+}
+
+.bottom-right {
+  @apply bottom-10 right-10;
+}
+</style>
